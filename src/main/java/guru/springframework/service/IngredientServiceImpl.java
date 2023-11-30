b5d5bc9919c3346066e9ba37d7255db3e16662bb
mehmet ali <bm.m.alicetin@gmail.com>
2023-11-30 21:13:18 +0300

    crud operations spring mvc

--- src/main/java/guru/springframework/service/IngredientServiceImpl.java
+++ src/main/java/guru/springframework/service/IngredientServiceImpl.java
@@ -13,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.Temporal;
 import java.beans.Transient;
 import java.util.HashSet;
 import java.util.Objects;
@@ -142,6 +143,4 @@ public class IngredientServiceImpl implements IngredientService{
 						.findFirst()
 						.orElseThrow());
 	}
-
-
 }
--- src/test/java/guru/springframework/controller/IndexControllerTest.java
+++ src/test/java/guru/springframework/controller/IndexControllerTest.java
@@ -15,8 +15,9 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.*;
 
@@ -68,5 +69,73 @@ public class IndexControllerTest {
 		Mockito.verify(recipeService, Mockito.times(5)).geRecipes();
 		Mockito.verify(model, Mockito.times(5))
 				.addAttribute(Mockito.eq("recipes"), Mockito.anySet());
+	}
+
+	@Test
+	public void testSpliterator(){
+		List<Integer> a = List.of(1,2,3,4,5);
+		Spliterator<Integer> spliterator = a.spliterator();
+		int characteristics = spliterator.characteristics();
+		long estimateSize = spliterator.estimateSize();
+		System.out.println("characteristics : "+characteristics);
+		System.out.println("estimateSize : "+estimateSize);
+		spliterator.forEachRemaining(System.out::println);
+		Spliterator<Integer> trySplit = spliterator.trySplit();
+		trySplit.forEachRemaining(System.out::println);
+	}
+
+	public class Article{
+		private List<Author> listOfAuthors;
+		private int id;
+		private String name;
+
+		public Article(String name) {
+			this.name = name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getName() {
+			return name;
+		}
+	}
+
+	public class Author{
+		private String name;
+	}
+
+	public void givenAStreamOfArticles_whenProcessedInSequentiallyWithSpliterator_ProducessRightOutput() {
+		List<Article> articles = Stream.generate(() -> new Article("Java"))
+				.limit(35000)
+				.collect(Collectors.toList());
+
+		Spliterator<Article> spliterator = articles.spliterator();
+		while (spliterator.tryAdvance(article -> article.setName(article.getName()
+				.concat("- published by Baeldung"))));
+
+		articles.forEach(article -> assertEquals(article.getName(),"Java- published by Baeldung"));
+	}
+
+	@Test
+	public void givenAStreamOfArticle_whenProcessedUsingTrySplit_thenSplitIntoEqualHalf() {
+		List<Article> articles = Stream.generate(() -> new Article("Java"))
+				.limit(35000)
+				.collect(Collectors.toList());
+
+		Spliterator<Article> split1 = articles.spliterator();
+
+		Spliterator<Article> split2 = split1.trySplit();
+
+		List<Article> articlesListOne = new ArrayList<>();
+		List<Article> articlesListTwo = new ArrayList<>();
+
+		split1.forEachRemaining(articlesListOne::add);
+		split2.forEachRemaining(articlesListTwo::add);
+
+		assertEquals(articlesListOne.size(), 17500);
+		assertEquals(articlesListTwo.size(),17500);
+
 	}
 }
